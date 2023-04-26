bc373fbc99676bdee45b64ab8e9e2f6a95cf5973
asakura42 <51290160+asakura42@users.noreply.github.com>
2023-04-26 18:20:14 +0000

    Update gd-sentence-line.c

--- gd-sentence-line.c
+++ gd-sentence-line.c
@@ -1,25 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 
+void print_usage(char *program_name) {
+  printf("Usage: %s %%GDSEARCH%% %%GDWORD%%\n", program_name);
+}
+
 int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        printf("Usage: %s %%GDSEARCH%% %%GDWORD%%\n", argv[0]);
-        return 1;
-    }
-    printf("<div class=\"gd-marisa\">");
-    char *word = strtok(argv[1], " ");
-    while (word != NULL) {
-        printf("<a href=\"bword:%s\"%s>%s</a> ", word, strcmp(word, argv[2]) == 0 ? " class=\"active\"" : "", word);
-        word = strtok(NULL, " ");
-    }
-    printf("</div>");
-    printf("<style>");
-    printf(".gd-marisa { font-size: 2rem; margin: 0.05em 0; color: #1268c3; }");
-    printf(".gd-marisa a { color: royalblue; text-decoration: none; border-bottom: dashed 2px currentColor; }");
-    printf(".gd-marisa a:hover { background-color: #ddeeff; }");
-    printf(".gd-marisa a.active { background-color: #f5f5f5; }");
-    printf(".gd-marisa a.active::before { content: '['; }");
-    printf(".gd-marisa a.active::after { content: ']'; }");
-    printf("</style>");
-    return 0;
+  if (argc < 3) {
+    print_usage(argv[0]);
+    return 1;
+  }
+
+  printf("<div class=\"gd-marisa\">\n");
+
+  char *word = strtok(argv[1], " ");
+  while (word != NULL) {
+    printf("<a href=\"bword:%s\"%s>%s</a> ", word,
+           strcmp(word, argv[2]) == 0 ? " class=\"active\"" : "", word);
+    word = strtok(NULL, " ");
+  }
+
+  printf("</div>\n");
+  printf("<style>\n");
+  printf(".gd-marisa {\n");
+  printf("  font-size: 2rem;\n");
+  printf("  margin: 0.05em 0;\n");
+  printf("  color: #1268c3;\n");
+  printf("}\n");
+  printf(".gd-marisa a {\n");
+  printf("  color: royalblue;\n");
+  printf("  text-decoration: none;\n");
+  printf("  border-bottom: dashed 2px currentColor;\n");
+  printf("}\n");
+  printf(".gd-marisa a:hover {\n");
+  printf("  background-color: #ddeeff;\n");
+  printf("}\n");
+  printf(".gd-marisa a.active {\n");
+  printf("  background-color: #f5f5f5;\n");
+  printf("}\n");
+  printf(".gd-marisa a.active::before {\n");
+  printf("  content: '[';\n");
+  printf("}\n");
+  printf(".gd-marisa a.active::after {\n");
+  printf("  content: ']';\n");
+  printf("}\n");
+  printf("</style>\n");
+
+  return 0;
 }
